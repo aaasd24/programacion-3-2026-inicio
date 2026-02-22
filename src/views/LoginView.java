d0ae2074a635f517036a956426bd3fb63a1cb9d0
aaasd24 <amro_24@alu.uabcs.mx>
2026-02-22 16:48:39 -0700

    avance de interfaz, falta agregar los textos bien

--- src/views/LoginView.java
+++ src/views/LoginView.java
@@ -1,6 +1,11 @@
 package views;
 import java.awt.BorderLayout;
+import java.awt.event.*;
+
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -8,11 +13,14 @@ import java.awt.Image;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 
@@ -26,54 +34,88 @@ public class LoginView extends JPanel{
 	
 	public LoginView() {
 		
-
-		Border emptyBorder = BorderFactory.createEmptyBorder(20,10,20,10);
-		setBorder(emptyBorder);
-		setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
+		//Definir el fondo principal
 		setBackground(azulGlaous);
+		setLayout(new BorderLayout());
+		//setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		//add(Box.createVerticalGlue());
+		
+		
+		//Crear 5 paneles para estructurar la primera faceta
+		JPanel panelSuperior = new JPanel();
+		panelSuperior.setBackground(Color.GREEN);
+		
+		JPanel panelCentral = new JPanel();
+		panelCentral.setBackground(Color.BLUE);
+		panelCentral.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		JPanel panelInferior = new JPanel();
+		panelInferior.setBackground(Color.RED);
+		
+		JPanel panelIzquierdo = new JPanel();
+		panelIzquierdo.setBackground(Color.PINK);
+		
+		JPanel panelDerecho = new JPanel();
+		panelDerecho.setBackground(Color.ORANGE);
+		
+		//Botones al panel superior
+		for (int i = 0; i < 10; i++) {
+			JButton b = new JButton(i + "");
+			panelInferior.add(b);
+			panelInferior.add(Box.createVerticalGlue());
+		}
+		
+		Border emptyBorder = BorderFactory.createEmptyBorder(10,10,10,10);
+		setBorder(emptyBorder);
+		
+		
+		add(panelSuperior, BorderLayout.NORTH);
+		add(panelDerecho, BorderLayout.LINE_END);
+		add(panelIzquierdo, BorderLayout.LINE_START);
+		add(panelInferior, BorderLayout.SOUTH);
+		add(panelCentral, BorderLayout.CENTER);
 		
 		
 		
-		//crearLogo();
-		//crearTexto();
-		//crearTexto();
-		//crearTexto("Ingrese su contraseña: ", 200, 350, 20);
-		//crearTexto("Error, credenciales incorectas, ingrese su INE", 200, 275, 15);
-		//crearTexto("Ingrese contraseña", 200, 430, 15);
-		//crearEntradaDeDatos();
-		//crearEntradaContrasenia();
-		//Boton();
-		
-		JLabel label = new JLabel("Hola");
-		label.setForeground(Color.black);
-		label.setFont(new Font("Arial", Font.PLAIN, 20));
-		this.add(label);
-		
-		JButton boton = new JButton("Button 1");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.5;
-		c.gridx = 0;
-		c.gridy = 0;
-		this.add(boton, c);
-		
-	}
-	
-	
-	
-	
-	
-	
-	public void crearTexto() {
-		
-		//add(label);
-	}
-	public void crearEntradaDeDatos() {
+		JLabel label1 = new JLabel("Hola");
+		JLabel label2 = new JLabel("Hola");
+		label1.setForeground(Color.black);
+		label1.setFont(new Font("Arial", Font.PLAIN, 20));
+		panelIzquierdo.add(label1);
+		panelDerecho.add(label2);
+
 		JTextField textoInicioCuenta = new JTextField();
 		textoInicioCuenta.setForeground(Color.BLACK);
 		textoInicioCuenta.setFont(new Font("Arial", Font.PLAIN, 30));
-		textoInicioCuenta.setBounds(200, 250, 400, 40);
-		add(textoInicioCuenta);
+		textoInicioCuenta.setSize(20, 20);
+		
+		c.gridheight = 3;
+		c.gridwidth = 3;
+		c.weightx = 4;
+		c.weighty = 2;
+		c.gridx = 2;
+		c.gridy = 0;
+		panelCentral.add(textoInicioCuenta, c);
+		
+		
+		try {
+	        Image img = ImageIO.read(getClass().getResource("../img/SteakGames.png"));
+	        Image imgEscalada = img.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+	        JLabel labelLogo = new JLabel(new ImageIcon(imgEscalada)); 
+	        panelSuperior.add(labelLogo);
+	        
+	    } catch (Exception ex) {
+	        System.out.println("No se encuentra la imagen");
+	    }
+	}
+	
+	public static void crearEntradaDeDatos(JPanel panel) {
+		JTextField textoInicioCuenta = new JTextField();
+		textoInicioCuenta.setForeground(Color.BLACK);
+		textoInicioCuenta.setFont(new Font("Arial", Font.PLAIN, 30));
+
+		panel.add(textoInicioCuenta);
 	}
 	
 	public void crearEntradaContrasenia() {
@@ -101,16 +143,4 @@ public class LoginView extends JPanel{
 		
 	}
 	
-	public void crearLogo() {
-	    try {
-	        Image img = ImageIO.read(getClass().getResource("../img/SteakGames.png"));
-	        Image imgEscalada = img.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
-	        JLabel labelLogo = new JLabel(new ImageIcon(imgEscalada));
-	        labelLogo.setBounds(300, 0, 150, 150); 
-	        add(labelLogo);
-	        
-	    } catch (Exception ex) {
-	        System.out.println("No se encuentra la imagen");
-	    }
-	}
 }
